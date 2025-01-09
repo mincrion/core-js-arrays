0fef04b7867f767d6cb11978ac6af17d6fd8721c
mincrion <mincrion@google.com>
2025-01-10 01:06:13 +0400

    feat: add getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -126,8 +126,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((tmp, item) => tmp + item, 0);
+  const average = sum / arr.length;
+  return Number(average.toFixed(2));
 }
 
 /**
